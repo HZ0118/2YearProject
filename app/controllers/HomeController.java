3b41ed9d049778f4b7752555445b7b96dcf28d45
Hailing Zhou <Hailing Zhou>
2017-04-17 15:46:08 +0100

    Added new java classes

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -31,48 +31,6 @@ public class HomeController extends Controller {
         return ok(index.render(getUserFromSession()));
     }
 
-    public Result flights() {
-        List<Flight> flightsList = Flight.findAll();
-        return ok(list.render(flightsList, User.getUserById(session().get("email"))));
-    }
-
-    public Result addFlight(){
-        Form<Flight> addFlightForm = FormFactory.form(Flight.class);
-        return ok(addFlight.render(addFlightForm, User.getUserById(session().get("email"))));
-    }
-
-    @Transactional
-    public Result addFlightSubmit(){
-        Form<Flight> newFlightForm = FormFactory.form(Flight.class).bindFromRequest();
-        if(newFlightForm.hasErrors()){
-            return badRequest(addFlight.render(newFlightForm, User.getUserById(session().get("email"))));
-        }
-        Flight newFlight = newFlightForm.get();
-            newFlight.save();
-        flash("success", "Flight " + newFlight.getID() + " has been created/ updated");
-        return redirect(controllers.routes.HomeController.flights());
-    }
-
-    @Transactional
-    public Result deleteFlight(int id){
-        Flight.find.ref(id).delete();
-        flash("success", "Flight has been deleted");
-        return redirect(routes.HomeController.flights());
-    }
-
-    @Transactional
-    public Result updateFlight(int id) {
-        Flight f;
-        Form<Flight> flightForm;
-        try{
-            f = Flight.find.byId(id);
-            flightForm = FormFactory.form(Flight.class).fill(f);
-        } catch (Exception ex) {
-            return badRequest("error");
-        }
-        return ok(addFlight.render(flightForm, User.getUserById(session().get("email"))));
-    }
-
     private User getUserFromSession() {
         return User.getUserById(session().get("email"));
     }
